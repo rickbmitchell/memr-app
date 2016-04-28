27f4be5b260ce3ce170b91a4af4a47c0814d7b17
Rick Mitchell <rickbmitchell@gmail.com]>
2016-04-28 16:11:39 -0400

    added pg

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'devise'
 gem 'omniauth-twitter', :github => 'arunagw/omniauth-twitter'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160419184804) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "archived_posts", force: :cascade do |t|
     t.integer  "post_id"
     t.integer  "archive_id"
@@ -84,8 +87,8 @@ ActiveRecord::Schema.define(version: 20160419184804) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "votes", force: :cascade do |t|
     t.integer  "user_id"
